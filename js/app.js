2709d2a1730b89374d2b069c27cf31e1acb81dad
Katy <katy.ledgard@gmail.com>
2023-06-29 15:19:07 +0100

    add function to save and get local data

--- js/app.js
+++ js/app.js
@@ -20,14 +20,23 @@ AppState.prototype.instantiateProducts = function () {
 
 AppState.prototype.saveToLocalStorage = function () {
   // TODO: Fill in this instance method to save product data to local storage
+  let savedProducts = JSON.stringify(this.allProducts);
+  localStorage.setItem("savedProducts", savedProducts);
+  // localStorage.setItem("productData", JSON.stringify(this.allProducts));
+  // console.log(this.allProducts);
 }
 
 AppState.prototype.loadItems = function () {
 
   // TODO: Update this instance method to retrieve data from local storage instead of creating new Products on each page load
+let getItems = JSON.parse(localStorage.getItem("savedProducts"))
 
+if (getItems) {
+  this.allProducts = getItems;
+} else {
   this.instantiateProducts();
-
+}
+console.log(getItems);
 }
 
 
@@ -37,3 +46,4 @@ function Product(name, fileExtension = 'jpg') {
   this.timesClicked = 0;
   this.timesShown = 0;
 }
+
--- js/chart.js
+++ js/chart.js
@@ -2,14 +2,23 @@
 
 let canvasElem = document.getElementById('chart')
 
-/* TODO:
- * - Instantiate a new AppState
- * - Use a method on that AppState to load vote data from localStorage.
- * - Create a data object for chart.js using your AppState's allProducts array.
- * - Combine the data object with configuration information for chart.js type, colors, etc
- * - Call chart.js with the configuration and the canvasElem
- *
- */
+//* TODO:
+ //* - Instantiate a new AppState
+
+ let chartData = new Appstate();
+
+//  * - Use a method on that AppState to load vote data from localStorage.
+chartData.loaditems();
+
+//  * - Create a data object for chart.js using your AppState's allProducts array.
+
+
+//  * - Combine the data object with configuration information for chart.js type, colors, etc
+
+
+//  * - Call chart.js with the configuration and the canvasElem
+
+ 
 function renderChart() {
 }
 
